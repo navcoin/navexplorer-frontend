6bed83993e2f396fffb804e993abf3bdad1ffaf7
Ruben Beeftink <ruben.beeftink@beeproger.com>
2018-06-25 22:26:38 +0200

    set active class based on url

--- assets/js/services/Nav.js
+++ assets/js/services/Nav.js
@@ -2,13 +2,25 @@ const $ = require('jquery');
 
 class Nav {
     constructor() {
-        $(function() {
-            $('.nav-item a').click(function() {
-                $(this).closest('.nav').find('.nav-item a').removeClass('active');
-                $(this).addClass('active');
-            });
-        });
-    }
+
+        this.setActiveClass();
+    };
+
+    setActiveClass() {
+
+        let currentPath = location.pathname;
+
+        $('.nav-item').each(function () {
+
+            let currentItem = $(this);
+            let currentLink = currentItem.find('.nav-link');
+
+            if (currentLink.attr('href') === currentPath) {
+
+                currentItem.addClass('active');
+            }
+        })
+    };
 }
 
 new Nav();
