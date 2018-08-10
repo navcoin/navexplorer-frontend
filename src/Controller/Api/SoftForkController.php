0188885911b687271da5c11d2e5c7c715931f767
dantudor <dtudor01@gmail.com>
2018-08-10 21:27:30 +0100

    Remove network switch from soft fork api

--- src/Controller/Api/SoftForkController.php
+++ src/Controller/Api/SoftForkController.php
@@ -22,14 +22,6 @@ class SoftForkController
      */
     public function getSoftForksAction(Request $request, SoftForkApi $softForkApi, SerializerInterface $serializer)
     {
-        switch ($request->headers->get('network')) {
-            case 'testnet':
-                $softForkApi->useNetwork(Network::TEST_NET);
-                break;
-            default:
-                $softForkApi->useNetwork(Network::MAIN_NET);
-        }
-
         $response = new Response($serializer->serialize($softForkApi->getAll()->getElements(), 'json'), 200);
         $response->headers->set('Content-Type', 'application/json');
 
