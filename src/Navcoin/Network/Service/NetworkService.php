edf830b00ec1c47a6216672475efc06d4d3fd550
dantudor <dtudor01@gmail.com>
2018-08-23 14:05:29 +0100

    Network service

---
+++ src/Navcoin/Network/Service/NetworkService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Navcoin\Network\Service;
+
+class NetworkService
+{
+    /**
+     * @var string
+     */
+    private $network;
+
+    public function __construct(string $network)
+    {
+        $this->network = $network;
+    }
+
+    public function isMainNet(): bool
+    {
+        return $this->network == "MAINNET";
+    }
+
+    public function isTestNet(): bool
+    {
+        return $this->network == "TESTNET";
+    }
+}
