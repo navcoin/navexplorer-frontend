2be3e385b9a4f735ac7773c844f1a0f9ea6031ef
dantudor <dtudor01@gmail.com>
2018-09-11 22:12:50 +0100

    Split community proposals page

--- src/Controller/CommunityFundController.php
+++ src/Controller/CommunityFundController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Navcoin\CommunityFund\Api\CommunityFundApi;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,19 +25,46 @@ class CommunityFundController extends Controller
      * @Route("/community-fund/proposals")
      * @Template()
      *
-     * @return array
+     * @return RedirectResponse
      */
-    public function indexAction()
+    public function redirectAction()
     {
+        return $this->redirectToRoute('app_communityfund_index', ['state' => 'pending']);
+    }
+
+    /**
+     * @Route("/community-fund/proposals/{state}")
+     * @Template()
+     *
+     * @return array|RedirectResponse
+     */
+    public function indexAction(Request $request)
+    {
+        switch($request->get('state')) {
+            case 'pending':
+                $proposals = $this->communityFundApi->getProposalsByState("PENDING", "votes");
+                break;
+            case 'accepted':
+                $proposals = $this->communityFundApi->getProposalsByState("ACCEPTED");
+                break;
+            case 'rejected':
+                $proposals = $this->communityFundApi->getProposalsByState("REJECTED");
+                break;
+            case 'expired':
+                $proposals = $this->communityFundApi->getProposalsByState("EXPIRED");
+                break;
+            case 'pending-funds':
+                $proposals = $this->communityFundApi->getProposalsByState("PENDING_FUNDS");
+                break;
+            default:
+                return $this->redirectToRoute('app_communityfund_index', ['state' => 'pending']);
+
+        }
+
         return [
             'blockCycle' => $this->communityFundApi->getBlockCycle(),
-            'proposals' => [
-                'pending' => $this->communityFundApi->getProposalsByState("PENDING", "votes"),
-                'accepted' => $this->communityFundApi->getProposalsByState("ACCEPTED"),
-                'rejected' => $this->communityFundApi->getProposalsByState("REJECTED"),
-                'expired' => $this->communityFundApi->getProposalsByState("EXPIRED"),
-                'pendingFunds' => $this->communityFundApi->getProposalsByState("PENDING_FUNDS"),
-            ]
+            'proposals' => $proposals,
+            'active' => $request->get('state'),
         ];
     }
 
