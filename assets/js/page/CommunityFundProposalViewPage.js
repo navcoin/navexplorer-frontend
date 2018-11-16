279b6b5b20c004f98b90cb4f9d9e206c2d727049
dantudor <dtudor01@gmail.com>
2018-11-16 10:21:44 +0000

    Live links in cfund proposals

---
+++ assets/js/page/CommunityFundProposalViewPage.js
@@ -0,0 +1,27 @@
+const $ = require('jquery');
+
+import anchorme from "anchorme";
+
+class CommunityFundProposalViewPage {
+    constructor()
+    {
+        console.log("CommunityFund Proposal View");
+
+        let $proposalDescription = $('.proposal-description');
+        $proposalDescription.html(
+            anchorme($proposalDescription.html(), {
+                truncate: 40,
+                attributes:[
+                    {
+                        name:"target",
+                        value:"_blank"
+                    }
+                ]
+            })
+        );
+    }
+}
+
+if ($('body').is('.page-communityfund-proposal-view')) {
+    new CommunityFundProposalViewPage();
+}
