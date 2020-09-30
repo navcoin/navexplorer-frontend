ba87824d0f67e3c0f193fe6f0a6b7573833390c6
dantudor <dtudor01@gmail.com>
2020-09-30 13:10:39 +0100

    homepage content

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,50 +2,45 @@
 
 namespace App\Controller;
 
+use App\Navcoin\Block\Api\BlockApi;
 use App\Navcoin\Block\Api\BlockGroupApi;
 use App\Navcoin\Common\Network;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends Controller
+class HomeController extends AbstractController
 {
-    /**
-     * @var BlockGroupApi
-     */
+    /**  @var BlockGroupApi */
     private $blockGroupApi;
 
-    public function __construct(BlockGroupApi $blockGroupApi)
+    /**  @var BlockApi */
+    private $blockApi;
+
+    public function __construct(BlockGroupApi $blockGroupApi, BlockApi $blockApi)
     {
         $this->blockGroupApi = $blockGroupApi;
+        $this->blockApi = $blockApi;
     }
 
     /**
      * @Route("/")
      * @Template()
-     *
-     * @param Request $request
-     *
-     * @return array
      */
     public function index(Request $request): array
     {
         return [
-            'blocks' => $this->blockGroupApi->getGroupByCategory($request->get('period', 'hourly'), 5)
+            'blocks' => $this->blockGroupApi->getGroupByCategory($request->get('period', 'hourly'), 6),
+            'best' => $this->blockApi->getBestBlock(),
         ];
     }
 
     /**
      * @Route("/network/{network}")
      * @Template()
-     *
-     * @param Request $request
-     * @param Session $session
-     *
-     * @return RedirectResponse
      */
     public function network(Request $request, Session $session): RedirectResponse
     {
