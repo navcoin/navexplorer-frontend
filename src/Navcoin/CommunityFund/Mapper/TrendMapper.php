8687678ee53f6d6a9e86eb2f915718fe4a0c2126
mxaddict <mxaddict@codedmaster.com>
2022-08-10 15:42:51 +0800

    Updated TrendMapper to give int 0 if data is not available

--- src/Navcoin/CommunityFund/Mapper/TrendMapper.php
+++ src/Navcoin/CommunityFund/Mapper/TrendMapper.php
@@ -12,14 +12,14 @@ class TrendMapper extends BaseMapper
         return new Trend(
             $data['start'],
             $data['end'],
-            $data['votes']['yes'],
-            $data['votes']['no'],
-            $data['votes']['abstain'],
-            $data['votes']['exclude'],
-            $data['trend']['yes'],
-            $data['trend']['no'],
-            $data['trend']['abstain'],
-            $data['trend']['exclude']
+            isset($data['votes']['yes']) ? $data['votes']['yes'] : 0,
+            isset($data['votes']['no']) ? $data['votes']['no'] : 0,
+            isset($data['votes']['abstain']) ? $data['votes']['abstain'] : 0,
+            isset($data['votes']['exclude']) ? $data['votes']['exclude'] : 0,
+            isset($data['trend']['yes']) ? $data['trend']['yes'] : 0,
+            isset($data['trend']['no']) ? $data['trend']['no'] : 0,
+            isset($data['trend']['abstain']) ? $data['trend']['abstain'] : 0,
+            isset($data['trend']['exclude']) ? $data['trend']['exclude'] : 0
         );
     }
 }
